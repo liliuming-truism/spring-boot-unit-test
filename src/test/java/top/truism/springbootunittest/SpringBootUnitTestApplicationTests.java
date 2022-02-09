9f11cbb7b43366f13bf8b4b2200ae9f8fc2e7133
Li <liuming.li@corp.winit.com>
2022-02-09 15:56:06 +0800

    Initial commit

---
+++ src/test/java/top/truism/springbootunittest/SpringBootUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package top.truism.springbootunittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUnitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
